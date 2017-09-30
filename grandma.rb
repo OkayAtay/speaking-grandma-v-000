dfea5031a17b52300bd40073394f651d7f5b8849
Araba Taylor <araba.taylor20@gmail.com>
2017-09-30 16:48:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string == string.upcase && "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!" && string.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     return  "NO, NOT SINCE 1938!"
